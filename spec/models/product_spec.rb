475ff61c9ac11db782b262fed1fe8f8e320546c3
RomanMedvedev91 <roman.medvedev77@gmail.com>
2022-01-11 00:07:30 +0000

    add validation test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,24 +3,30 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
+    it 'should save successfuly' do
+      @product = Product.new({name: "Name", price: 10, quantity: 2, category_id: 2})
+      @product.save
+      expect(@product).to be_present
+    end
+
     it 'should have name' do
-      @product = Product.new({name: "name"})
-      expect(@product.name).to be_present
+      @product = Product.new({name: nil, price: 10, quantity: 5, category_id: 2})
+      expect(@product.name).to eql(nil), @product.errors.full_messages
     end
 
     it 'should have price' do
-      @product = Product.new({price: 10})
-      expect(@product.price).to be_present
+      @product = Product.new({name: "Name", price_cents: nil, quantity: 2, category_id: 2})
+       expect(@product.price_cents).to eql(nil), @product.errors.full_messages
     end
     
     it 'should have quantity' do
-      @product = Product.new({quantity: 2})
-      expect(@product.quantity).to be_present
+      @product = Product.new({name: "Name", price: 10, quantity: nil, category_id: 2})
+      expect(@product.quantity).to eql(nil), @product.errors.full_messages
     end
     
-    it 'should have category' do
-      @product = Product.new({category_id: 2})
-      expect(@product.category_id).to be_present
+    it 'should have category equal to nil' do
+      @product = Product.new({name: "Name", price: 10, quantity: 2, category_id: nil})
+       expect(@product.category_id).to eql(nil), @product.errors.full_messages
     end
     
   end
